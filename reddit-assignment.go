27f90a50333a0bf27171cde26e06a1101e933655
Gabriel Forien <gforien@users.noreply.github.com>
2021-12-12 13:39:58 +0100

    Create reddit-assignment.go

---
+++ reddit-assignment.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	counter := 0
+
+	// Get counter
+	r.GET("/count", func(c *gin.Context) {
+		c.String(http.StatusOK, "%d", counter)
+	})
+
+	// Increment counter
+	r.POST("/inc", func(c *gin.Context) {
+		counter++
+		c.String(http.StatusOK, "%d", counter)
+	})
+
+	// Decrement counter
+	r.POST("/dec", func(c *gin.Context) {
+		counter--
+		c.String(http.StatusOK, "%d", counter)
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":5000")
+}
