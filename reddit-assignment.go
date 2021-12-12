6569c5978da252183fb55a4caea4335ee856cf50
Gabriel Forien <gforien+dev@gmail.com>
2021-12-12 16:02:41 +0100

    Client Redis ajouté dans une fonction indépendante

--- reddit-assignment.go
+++ reddit-assignment.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"net/http"
 )
 
 var db = make(map[string]string)
+var redisContext = context.Background()
 
 func setupHTTPServer() *gin.Engine {
 	app := gin.Default()
@@ -37,9 +40,41 @@ func setupHTTPServer() *gin.Engine {
 	return app
 }
 
+func setupRedisClient(addr string) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := client.Set(redisContext, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get(redisContext, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get(redisContext, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+
+	return client
+}
+
 func main() {
-	r := setupRouter()
-	r.Run(":5000")
+	_ = setupRedisClient("localhost:6379")
+
 	app := setupHTTPServer()
 	app.Run(":5000")
 }
