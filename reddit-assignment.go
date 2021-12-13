00dfba87390bbf3e20c2857a86b0f45742fc6814
Gabriel Forien <gforien+dev@gmail.com>
2021-12-13 09:51:59 +0100

    On renomme 'client' en 'rdb' pour plus de clarté

--- reddit-assignment.go
+++ reddit-assignment.go
@@ -12,10 +12,10 @@ import (
 var db = make(map[string]string)
 var redisContext = context.Background()
 
-func setupHTTPServer(client *redis.Client) *gin.Engine {
+func setupHTTPServer(rdb *redis.Client) *gin.Engine {
 	app := gin.Default()
 
-	err := client.Set(redisContext, "count", 0, 0).Err()
+	err := rdb.Set(redisContext, "count", 0, 0).Err()
 	if err != nil {
 		panic(err)
 	}
@@ -27,7 +27,7 @@ func setupHTTPServer(client *redis.Client) *gin.Engine {
 
 	// Get counter
 	app.GET("/count", func(c *gin.Context) {
-		sVal, err := client.Get(redisContext, "count").Result()
+		sVal, err := rdb.Get(redisContext, "count").Result()
 		if err != nil {
 			panic(err)
 		}
@@ -40,7 +40,7 @@ func setupHTTPServer(client *redis.Client) *gin.Engine {
 
 	// Increment counter
 	app.POST("/inc", func(c *gin.Context) {
-		val, err := client.Incr(redisContext, "count").Result()
+		val, err := rdb.Incr(redisContext, "count").Result()
 		if err != nil {
 			panic(err)
 		}
@@ -49,7 +49,7 @@ func setupHTTPServer(client *redis.Client) *gin.Engine {
 
 	// Decrement counter
 	app.POST("/dec", func(c *gin.Context) {
-		val, err := client.Decr(redisContext, "count").Result()
+		val, err := rdb.Decr(redisContext, "count").Result()
 		if err != nil {
 			panic(err)
 		}
@@ -60,24 +60,24 @@ func setupHTTPServer(client *redis.Client) *gin.Engine {
 }
 
 func setupRedisClient(addr string) *redis.Client {
-	client := redis.NewClient(&redis.Options{
+	rdb := redis.NewClient(&redis.Options{
 		Addr:     addr,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	err := client.Set(redisContext, "key", "value", 0).Err()
+	err := rdb.Set(redisContext, "key", "value", 0).Err()
 	if err != nil {
 		panic(err)
 	}
 
-	val, err := client.Get(redisContext, "key").Result()
+	val, err := rdb.Get(redisContext, "key").Result()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("key", val)
 
-	val2, err := client.Get(redisContext, "key2").Result()
+	val2, err := rdb.Get(redisContext, "key2").Result()
 	if err == redis.Nil {
 		fmt.Println("key2 does not exist")
 	} else if err != nil {
@@ -88,12 +88,12 @@ func setupRedisClient(addr string) *redis.Client {
 	// Output: key value
 	// key2 does not exist
 
-	return client
+	return rdb
 }
 
 func main() {
-	client := setupRedisClient("localhost:6379")
+	rdb := setupRedisClient("localhost:6379")
 
-	app := setupHTTPServer(client)
+	app := setupHTTPServer(rdb)
 	app.Run(":5000")
 }
